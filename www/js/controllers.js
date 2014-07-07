20e0b4cb3bd3e343e797ab7bc6b7593811f832b0
Bryan Villafañe <briananime07@gmail.com>
2014-07-07 18:05:38 -0500

    changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,5 @@
 angular.module('app.controllers', [])  
   
-  .controller('AppCtrl', function ($scope, $state) {
-    $scope.logout = function () {
-        OpenFB.logout();
-        $state.go('app.login');
-    };
-  });
-
   .controller('PidoCtrl', function($scope, $state) {
     $scope.mePido = function(id) {
       console.log('Pido', id);
